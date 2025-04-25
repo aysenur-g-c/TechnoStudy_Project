31776bbcd0e63dc8603d779a2be45c1d25172e47
ennur7 <ennur789@gmail.com>
2025-04-25 21:21:20 +0300

    TC02 is done

---
+++ src/TC02/TC02.java
@@ -0,0 +1,25 @@
+package TC02;
+
+import Utility.BaseDriver;
+import Utility.Elements;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class TC02 extends BaseDriver {
+    @Test(groups = "Smoke,Regression")
+
+    public void US_502() {
+        driver.get("https://techno.study/");
+
+        Elements elements=new Elements(driver);
+        Assert.assertEquals("Yanlış sayfaya gidildi!", "https://techno.study/", driver.getCurrentUrl());
+        wait.until(ExpectedConditions.visibilityOf(elements.signInButton)).click();
+        wait.until(ExpectedConditions.urlToBe("https://campus.techno.study/"));
+
+
+
+
+    }
+
+}

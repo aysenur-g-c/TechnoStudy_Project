63d0e3c7458fa368f9f33802a375aad8a5587fb1
ahyat <gokdemir.aysenur@gmail.com>
2025-04-25 20:46:36 +0300

    elements sayfası güncellendi

--- src/Utility/Elements.java
+++ src/Utility/Elements.java
@@ -12,6 +12,49 @@ public class Elements {
     public Elements(WebDriver driver) {
         PageFactory.initElements(driver,this);    }
 
+    @FindBy(xpath = "//*[text()='APPLY NOW']")
+    public WebElement applyNowButton;
 
+    @FindBy(xpath = "//*[@placeholder='Full name']")
+    public WebElement fullNameInputField;
+
+    @FindBy(xpath = "//*[@placeholder='Email']")
+    public WebElement emailInputField;
+
+    @FindBy(xpath = "//*[@class='t-input-phonemask__select-flag']")
+    public WebElement phoneFlagSelect;
+
+    @FindBy(xpath = "//*[@class='t-input t-input-phonemask']")
+    public WebElement phoneNumberInputField;
+
+    @FindBy(xpath = "//*[@name='country']")
+    public WebElement countySelect;
+
+    @FindBy(xpath = "//*[@name='course']")
+    public WebElement courseSelect;
+
+    @FindBy(xpath = "//*[@name='survey']")
+    public WebElement howDidYouButton;
+
+    @FindBy(xpath = "//*[@name='promo code']")
+    public WebElement promoCodeInputField;
+
+    @FindBy(xpath = "//*[@name='Checkbox']//following::div")
+    public WebElement agreeCheckBox;
+
+    @FindBy(xpath = "//*[text()='Book a Call']")
+    public WebElement bookACallButton;
+
+    @FindBy(xpath = "//*[@id='tildaformsuccesspopuptext']")
+    public WebElement successMessage;
+
+    @FindBy(xpath = "//*[@class='t-form__errorbox-item']")
+    public List<WebElement> errorMessages;
+
+    @FindBy (xpath = "(//*[@class='t228__list_item'])[4]")
+    public WebElement blogsButton;
+
+    @FindBy (xpath = "//*[@class='js-feed t-feed t-feed_col']/ul")
+    public List<WebElement> blogsList;
 
 }

d711c1e3b930401c6a340b1c2af83156e01b4ecb
ahyat <gokdemir.aysenur@gmail.com>
2025-04-29 20:25:07 +0300

    Log eklendi

--- src/Utility/BaseDriverParameter.java
+++ src/Utility/BaseDriverParameter.java
@@ -1,13 +1,12 @@
 package Utility;
 
-import org.openqa.selenium.By;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -15,14 +14,16 @@ import org.testng.annotations.Parameters;
 import java.time.Duration;
 
 public class BaseDriverParameter {
-    // aşağısını SDET8 den aldık
+    public static Logger LogTutma = LogManager.getLogger();
+
     public WebDriver driver;
     public WebDriverWait wait;
 
     @BeforeClass
     @Parameters("BrowserTipi")
     public void Setup(String browserTipi) {
-        // seleniumdaki static yani başlangıç kısımlar konacak
+        LogTutma.info("Log Tutma işlemi Başladı");
+
         switch (browserTipi.toLowerCase()) {
             case "firefox":
                 driver = new FirefoxDriver();
@@ -40,6 +41,8 @@ public class BaseDriverParameter {
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
+        LogTutma.info("Başlangıç değişkenleri driver, wait, log tanımlandı ve başlatıldı");
+
     }
 
 
@@ -48,6 +51,9 @@ public class BaseDriverParameter {
         // seleniumdaki BekleKapat
         MyFunc.Bekle(3);
         driver.quit();  // bütün açılmış windowları kapatır
+        LogTutma.info("Driver kapatıldı");
+
+        LogTutma.warn("Driver kapatılamadı.");
     }
 
 
